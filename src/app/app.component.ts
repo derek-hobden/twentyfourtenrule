2f82aacc6e47d9b8dfc161a649372d748adf75be
Derek Hobden <dhobdensa@gmail.com>
2018-07-08 20:45:57 +0400

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'twentyfourtenrule.com';
+  monthlySalary = 0;
+  monthlyPayments = 0;
+  monthlyPetrol = 0;
+  monthlyTollFees = 0;
+  annualServices = 0;
+  annualInsurance = 0;
+
+  onSalaryKeyup(e) {
+    this.monthlySalary = parseInt(e.target.value, 10) || 0;
+  }
+
+  onMonthlyPaymentKeyup(e) {
+    this.monthlyPayments = parseInt(e.target.value, 10) || 0;
+  }
+
+  onMonthlyPetrolKeyup(e) {
+    this.monthlyPetrol = parseInt(e.target.value, 10) || 0;
+  }
+
+  onMonthlyTollFeesKeyup(e) {
+    this.monthlyTollFees = parseInt(e.target.value, 10) || 0;
+  }
+
+  onAnnualServicesKeyup(e) {
+    this.annualServices = parseInt(e.target.value, 10) || 0;
+  }
+
+  onAnnualInsuranceKeyup(e) {
+    this.annualInsurance = parseInt(e.target.value, 10) || 0;
+  }
+
+  get limit(): number {
+    return Math.round(this.monthlySalary / 10);
+  }
+
+  get totalMonthlyFees(): number {
+    return Math.round(
+      this.monthlyPayments +
+      this.monthlyPetrol +
+      this.monthlyTollFees +
+      (this.annualServices / 12) +
+      (this.annualInsurance / 12)
+    );
+  }
 }
